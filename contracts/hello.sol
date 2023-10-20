32adf966c5e1859b74a08bd0e94e88012d6d2e55
muazgh1 <muazibrahimgh@gmail.com>
2023-10-20 12:44:45 +0000

    hello world

---
+++ contracts/hello.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Main {
+    string name;
+
+    function setUserName(string memory userName) public {
+        name = userName;
+    }
+
+    function printMessage() public returns(string memory) {
+        bytes memory concat_strings;
+        concat_strings = abi.encodePacked("Hello ");
+        concat_strings = abi.encodePacked(concat_strings, name);
+        concat_strings = abi.encodePacked(concat_strings, "!");
+        string memory message = string(concat_strings);
+
+        return message;
+
+    }
+}
